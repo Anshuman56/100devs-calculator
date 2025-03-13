ee781359f774f2174ef76d4a53e86d7b584c01f1
Anshuman56 <anshumandehury@yandex.com>
2025-03-12 22:10:20 -0700

    add a clear button and change the eval function to general logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,42 @@
 let obj = {};
+obj.first = "";
+obj.second = "";
+obj.opration = "";
 obj.input = document.querySelector("input");
 obj.toDisplay = function (item) {
   this.input.value += item;
 };
+obj.clear = function () {
+  this.input.value = "";
+};
+obj.toOparation = function (item) {
+  obj.first = this.input.value;
+  obj.input.value = "";
+  if (item === "+") {
+    obj.opration = item;
+  }
+  if (item === "-") {
+    obj.opration = item;
+  }
+  if (item === "*") {
+    obj.opration = item;
+  }
+  if (item === "/") {
+    obj.opration = item;
+  }
+};
 obj.calculate = function () {
-  this.input.value = eval(this.input.value);
+  obj.second = this.input.value;
+  if (obj.opration === "+") {
+    this.input.value = Number(obj.first) + Number(obj.second);
+  }
+  if (obj.opration === "-") {
+    this.input.value = Number(obj.first) - Number(obj.second);
+  }
+  if (obj.opration === "*") {
+    this.input.value = Number(obj.first) * Number(obj.second);
+  }
+  if (obj.opration === "/") {
+    this.input.value = Number(obj.first) / Number(obj.second);
+  }
 };
